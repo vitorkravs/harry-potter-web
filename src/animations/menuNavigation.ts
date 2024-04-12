01fe182a3d58a693563ab87cadd4441296b87d9e
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-11 23:45:13 -0300

    feat: criando animação do menu

---
+++ src/animations/menuNavigation.ts
@@ -0,0 +1,14 @@
+export const menuAnimation = {
+  opened: {
+    opacity: 1,
+    transition: {
+      stiffness: 10,
+    },
+  },
+  closed: {
+    opacity: 0,
+    transition: {
+      stiffness: 10,
+    },
+  },
+};
