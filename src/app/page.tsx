067ae21f411bf1d0579cb37ff87995f693a4a45d
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-16 16:15:21 -0300

    feat: adicionando componente Movies na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Characters from "@/components/Characters/Characters";
 import Header from "@/components/Header/Header";
+import Movies from "@/components/Movies/Movies";
 import Welcome from "@/components/Welcome/Welcome";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Welcome />
         <Characters />
+        <Movies />
       </main>
     </>
   );
