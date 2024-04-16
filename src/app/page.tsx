52ab3ee6b49d1c23ff7d6dfe787e6597a4fb7ed6
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-16 18:32:29 -0300

    feat: adicionando componente Potions na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Characters from "@/components/Characters/Characters";
 import Header from "@/components/Header/Header";
 import Movies from "@/components/Movies/Movies";
+import Potions from "@/components/Potions/Potions";
 import Welcome from "@/components/Welcome/Welcome";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <Welcome />
         <Characters />
         <Movies />
+        <Potions />
       </main>
     </>
   );
