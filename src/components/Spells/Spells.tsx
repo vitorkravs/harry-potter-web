52651b2360b7c4b6aa1f5998e18519a7f92452ba
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-17 00:47:47 -0300

    feat: criando estrutura inicial do componente Spells

---
+++ src/components/Spells/Spells.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+//style
+import "./styles.scss";
+
+//requisição
+import axios from "axios";
+
+//hooks
+import { useEffect, useState } from "react";
+
+interface Spells {
+  data: [];
+
+  attributes: {
+    slug: string;
+    category: string;
+    effect: string;
+    image: string;
+  };
+}
+
+const Spells = () => {
+  const [spells, setSpells] = useState<Spells[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    const url = "https://api.potterdb.com/v1/spells";
+    axios
+      .get<Spells>(url)
+      .then((response) => {
+        setSpells(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Erro na requisição:", error);
+      });
+  }, []);
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredSpells = spells.filter((spell: Spells) =>
+    spell.attributes.slug.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const firstFilteredSpell =
+    filteredSpells.length > 0 ? filteredSpells[0] : null;
+
+  return (
+    <section id="spells-container">
+      <div className="spell-title">
+        <h2>Feitiços</h2>
+      </div>
+      <div className="input-search-spell">
+        <input
+          type="text"
+          placeholder="Digite o nome do personagem aqui"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+      <div className="spell-list">
+        {firstFilteredSpell ? (
+          <div className="spell-card">
+            <h3>{firstFilteredSpell.attributes.slug}</h3>
+            <picture>
+              <img
+                src={
+                  (Array.isArray(firstFilteredSpell.attributes.image) &&
+                    firstFilteredSpell.attributes.image.length === 0) ||
+                  firstFilteredSpell.attributes.image === null
+                    ? "/hogwarts-logo.jpg"
+                    : firstFilteredSpell.attributes.image
+                }
+                alt={firstFilteredSpell.attributes.slug}
+                width={180}
+                height={200}
+              />
+            </picture>
+            <p>
+              {firstFilteredSpell.attributes.effect
+                ? `Efeitos: ${firstFilteredSpell.attributes.effect}`
+                : null}
+            </p>
+          </div>
+        ) : (
+          <div
+            style={{
+              background: "rgba(255, 255, 255, 0.5)",
+              padding: ".5rem 1rem",
+              borderRadius: "1.5rem",
+              marginTop: "2rem",
+            }}
+          >
+            <p>Feitiço não encontado :{"("}</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Spells;
