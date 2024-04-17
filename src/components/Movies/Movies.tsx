4c3c5c4d1be871d55be3a77f6b1a0c5af514e0d1
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-17 15:20:08 -0300

    feat: resolvendo bug da animação

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -132,9 +132,9 @@ const Movies = () => {
           </picture>
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, x: 100 }}
+          initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 100 }}
+          exit={{ opacity: 0, x: -100 }}
           transition={{ duration: 1 }}
           className="movies-content-two"
         >
