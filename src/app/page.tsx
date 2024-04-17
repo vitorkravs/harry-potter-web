47f76514593886d05bfe61e7ea407fef736410fc
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-17 01:30:45 -0300

    feat: adicionando componente ScrollToTopButton na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Movies from "@/components/Movies/Movies";
 import Potions from "@/components/Potions/Potions";
+import ScrollToTopButton from "@/components/ScrollToTopButton/ScrollToTopButton";
 import Spells from "@/components/Spells/Spells";
 import Welcome from "@/components/Welcome/Welcome";
 
 export default function Home() {
   return (
     <>
+      <ScrollToTopButton />
       <Header />
       <main>
         <Welcome />
