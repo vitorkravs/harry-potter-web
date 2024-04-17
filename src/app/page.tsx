ddda1a99a2f88998a8d72c05793483c482f2c7d3
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-17 00:48:23 -0300

    feat: adicionando componente Spells na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Characters from "@/components/Characters/Characters";
 import Header from "@/components/Header/Header";
 import Movies from "@/components/Movies/Movies";
 import Potions from "@/components/Potions/Potions";
+import Spells from "@/components/Spells/Spells";
 import Welcome from "@/components/Welcome/Welcome";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <Characters />
         <Movies />
         <Potions />
+        <Spells />
       </main>
     </>
   );
